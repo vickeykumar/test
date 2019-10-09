f1cac7824607b7560eda807cd4877b1b34c31d6d
vickeykumar <kumarvickey45@gmail.com>
2019-10-09 20:47:34 +0530

    firebase job2

--- firebase_job.py
+++ firebase_job.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import pyrebase
+from datetime import datetime
+fmt="%d/%m/%Y %H:%M:%S"
 firebaseconfig = {
         "apiKey": "AIzaSyCcHshsJr7GXkyIaFOFRgfu76mS9eEUHOA",
         "authDomain": "root-grammar-251415.firebaseapp.com",
@@ -13,7 +15,7 @@ db = firebase.database()
 try:
     keys = db.get().val().keys()
     for key in keys:
-        print "removing: ",key
+        print datetime.now().strftime(fmt),"removing: ",key
         db.child(key).remove()
 except Exception,e:
-    print "Exception: ",e
+    print datetime.now().strftime(fmt),"Exception: ",e
