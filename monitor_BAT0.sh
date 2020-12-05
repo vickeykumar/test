07ecda901ce90d9475b55e8f30c05bec716fdf3b
vickeykumar <vickeykumar@github.com>
2020-12-05 20:13:50 +0530

    battery monitor 1

--- monitor_BAT0.sh
+++ monitor_BAT0.sh
@@ -2,11 +2,15 @@
 
 echo "                 Monitoring battery level";
 i=`cat /sys/class/power_supply/BAT0/capacity`
+stat=`cat /sys/class/power_supply/BAT0/status`
 echo "current capacity : $i, " "target capacity : $1"
-
-if [ $i -le $1 ];
+shopt -s nocasematch; 
+if [[ "$stat" = "Discharging" ]];
 then
-	notify-send --icon=error "Power Critical";
-	paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga;
+	if [ $i -le $1 ];
+	then
+		notify-send --icon=error "Power Critical";
+		paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga;
+	fi
 fi
-
+shopt -u nocasematch;
